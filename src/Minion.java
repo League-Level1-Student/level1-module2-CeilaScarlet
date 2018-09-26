3d061d82c44213c2e93b55fac4d208ac0e993ec1
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-09-25 19:23:26 -0700

    PixalRunner.java-src/extra
    Platypus-src/extra
    Platypus.java-src/extra
    PlatypusRunner.java-src/extra
    .gitignore-bin
    Minion.java-src
    MinionTest.class-bin

--- src/Minion.java
+++ src/Minion.java
@@ -1,9 +1,38 @@
 
 public class Minion {
-public static void main(String[] args) {
-class Minion (String name) {
-	
-}
-	
+	private String name; 
+	private int eyes; 
+	private String color; 
+	private String master;
+	Minion(String n, int e, String c,String m){
+		name = n;
+		eyes = e;
+		color = c;
+		master = m;
+	}
+	public String getName() {
+		// TODO Auto-generated method stub
+	return name;
+
+	}
+	public int getEyes() {
+		// TODO Auto-generated method stub
+	return eyes;
+
+	}
+	public String getColor() {
+		// TODO Auto-generated method stub
+	return color;
+
+	}
+	public String getMaster() {
+		// TODO Auto-generated method stub
+	return master;
+
+	}
+	public void setMaster(String master) {
+		// TODO Auto-generated method stub
+	this.master = master;
+
 }
 }
---
+++ src/extra/Platypus.java
@@ -0,0 +1,11 @@
+package extra;
+
+public class Platypus {
+	      private String name;
+
+	      	void sayHi(){
+	            	System.out.println("The platypus " + name + " is smarter than your average platypus.");
+	      	}
+	}
+
+
