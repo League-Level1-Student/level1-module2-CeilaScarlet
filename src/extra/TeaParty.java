8476d1c4c7124520ec56108b271e2b0ed01c2f6d
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-09 19:19:22 -0700

    cp
    TeaPartyTest.java-src/extra
    .gitignore-bin
    TeaParty.java-src/extra

--- src/extra/TeaParty.java
+++ src/extra/TeaParty.java
@@ -1,10 +1,28 @@
 package extra;
 
+import javax.swing.JOptionPane;
+
 public class TeaParty {
 
 	public String welcome(String name, boolean isWoman, boolean isKnighted) {
 	    
-	String welcome = "";
+	String welcome = "Hello ";
 	
+	if (isKnighted) {
+		if (isWoman) {
+			welcome += "Lady ";
+		}else {
+			welcome+= "Sir ";
+		}
+	}else {
+		if (isWoman) {
+			welcome += "Ms. ";
+		}else {
+			welcome+= "Mr. ";
+		}
+	}
+	welcome += name;
+	
+	return welcome;
 }
 }
